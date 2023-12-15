7a35cd8b84f15756f6eb48d2a436d5d7e3c73d4d
rajeshgithub12 <153889863+rajeshgithub12@users.noreply.github.com>
2023-12-14 21:42:44 -0800

    Add files via upload

---
+++ pgm3.py
@@ -0,0 +1,14 @@
+#program 2
+
+queue=[]
+queue.append("a")
+#queue.append("b")
+#queue.append("c")
+print('initial queue')
+print("\nElements from dequeued from queue")
+print(queue.pop())
+print(queue.pop())
+print(queue.pop())
+print("\nQueue after removing elements")
+
+
